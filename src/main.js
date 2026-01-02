8ee9c8ff1d713cbfef1cecb249fa931188121045
Vexel-Lab <vexel@example.com>
2026-01-03 00:15:38 +0900

    s

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Phaser from 'phaser';
-// 【修正】 ./scenes/... ではなく ./... からインポートする
+
+// 1. 各ファイルからクラスをインポート（パスは './ファイル名'）
 import { OpeningScene, TutorialScene, WorldScene, ShopScene, SkillScene, NormalClearScene, SecretBossIntroScene, TrueClearScene } from './MenuScenes.js';
 import { BattleScene } from './BattleScene.js';
 
-// シーン登録
+// 2. ゲームの設定
 const config = {
   type: Phaser.AUTO,
   width: 400,
@@ -15,6 +16,7 @@ const config = {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
+  // 3. ここで読み込むシーンをリスト化
   scene: [
     OpeningScene,
     TutorialScene,
@@ -28,4 +30,5 @@ const config = {
   ]
 };
 
+// 4. ゲーム開始
 new Phaser.Game(config);
